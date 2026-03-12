0fe972f8fe9360a65a55f0193732e20c3f4dcdd5
Javier-Becerra <javier.becerra@usach.cl>
2026-03-11 22:53:55 -0300

    Update DatosRegistroTopico.java
    adding validations

--- src/main/java/com/javier/forohub/dto/DatosRegistroTopico.java
+++ src/main/java/com/javier/forohub/dto/DatosRegistroTopico.java
@@ -1,3 +1,14 @@
 package com.javier.forohub.dto;
 
-public record DatosRegistroTopico(String titulo, String mensaje, String autor, String curso) {}
+import jakarta.validation.constraints.NotBlank;
+
+public record DatosRegistroTopico(
+    @NotBlank(message = "El título es obligatorio") 
+    String titulo, 
+    @NotBlank(message = "El mensaje no puede estar vacío") 
+    String mensaje, 
+    @NotBlank(message = "El autor es obligatorio") 
+    String autor, 
+    @NotBlank(message = "El curso es obligatorio") 
+    String curso
+) {}
