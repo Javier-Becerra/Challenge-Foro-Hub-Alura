a1700a23bd654cfd3bf2bb1a230e5078622f6d80
Javier-Becerra <javier.becerra@usach.cl>
2026-03-11 23:21:24 -0300

    Update RepositorioTopico.java
    Addition of duplicate verification

--- src/main/java/com/javier/forohub/repositorio/RepositorioTopico.java
+++ src/main/java/com/javier/forohub/repositorio/RepositorioTopico.java
@@ -3,4 +3,8 @@ package com.javier.forohub.repositorio;
 import com.javier.forohub.dominio.topico.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RepositorioTopico extends JpaRepository<Topico, Long> {}
+public interface RepositorioTopico extends JpaRepository<Topico, Long> {
+    
+    // Este método verifica si ya existe la combinación exacta en la BD
+    boolean existsByTituloAndMensaje(String titulo, String mensaje);
+}
